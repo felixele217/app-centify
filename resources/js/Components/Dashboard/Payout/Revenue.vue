a27ab7ec2541a87e89ed5c6db223df6955194444
Paul Sochiera <paul@sochiera.com>
2023-06-03 13:53:04 +0200

    added icon to button

--- resources/js/Components/Dashboard/Payout/Revenue.vue
+++ resources/js/Components/Dashboard/Payout/Revenue.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
+import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
 import Card from '../../Card.vue'
 import { highlightedText, subtleText } from '../styles'
 </script>
@@ -11,7 +12,13 @@ import { highlightedText, subtleText } from '../styles'
                 <p :class="subtleText">ARR acquired</p>
                 <p :class="highlightedText">260.476€</p>
             </div>
-            <PrimaryButton text="QTD" />
+            <PrimaryButton>
+                QTD
+                <ChevronDownIcon
+                    class="ml-2 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                />
+            </PrimaryButton>
         </div>
 
         <div>
