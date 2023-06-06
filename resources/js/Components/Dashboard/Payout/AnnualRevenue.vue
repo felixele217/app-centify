b0de03ff820625af0925fc5246518e151f26e5de
Paul Sochiera <paul@sochiera.com>
2023-06-06 12:30:09 +0200

    remove: unneccessary button

--- resources/js/Components/Dashboard/Payout/AnnualRevenue.vue
+++ resources/js/Components/Dashboard/Payout/AnnualRevenue.vue
@@ -22,14 +22,6 @@ import Trend from '../Trend.vue'
                     aria-hidden="true"
                 />
             </PrimaryButton>
-
-            <SecondaryButton padding="px-2 h-8">
-                QTD
-                <ChevronDownIcon
-                    class="ml-2 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                />
-            </SecondaryButton>
         </div>
 
         <div class="flex justify-between">
