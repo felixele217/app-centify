376090fe1b17a4182f008bbc527bdd72b6011382
felixele217 <felix.droese@gmx.net>
2023-07-02 14:47:42 +0200

    feat: remove focus from agent select

--- resources/js/Components/Form/MultiSelect.vue
+++ resources/js/Components/Form/MultiSelect.vue
@@ -19,7 +19,7 @@ function isSelected(optionId: number) {
     <Listbox as="div" multiple>
         <div class="relative mt-2">
             <ListboxButton
-                class="relative w-full cursor-pointer rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class="relative w-full cursor-pointer rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6"
             >
                 <span class="block truncate text-gray-300">Assign to agents...</span>
                 <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
--- resources/js/Components/Form/SelectWithDescription.vue
+++ resources/js/Components/Form/SelectWithDescription.vue
@@ -18,6 +18,7 @@ const selected = ref<null | {
     description: string
     current: boolean
 }>(null)
+
 </script>
 
 <template>
@@ -28,10 +29,12 @@ const selected = ref<null | {
         <ListboxLabel class="sr-only">Change published status</ListboxLabel>
 
         <div
-            class="relative mt-1 flex w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            class="relative mt-1 flex w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
         >
-            <ListboxButton class="inline-flex grow divide-x divide-gray-300">
-                <div class="inline-flex grow items-center gap-x-1.5 px-3 py-2">
+            <ListboxButton
+                class="inline-flex grow divide-x divide-gray-300"
+            >
+                <div class="inline-flex grow items-center gap-x-1.5 px-3 py-2 focus:ring-2 focus:ring-inset">
                     <CheckIcon
                         class="-ml-0.5 h-5 w-5"
                         :class="selected ? 'text-gray-600' : 'text-gray-300'"
