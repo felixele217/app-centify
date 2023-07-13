b1d0d230108a24f9cf7879b256506ed668b83607
felixele217 <felix.droese@gmx.net>
2023-07-13 11:06:56 +0200

    feat: it throws an exception if the demo set by api is wrong

--- app/Integrations/Pipedrive/PipedriveHelper.php
+++ app/Integrations/Pipedrive/PipedriveHelper.php
@@ -2,11 +2,17 @@
 
 namespace App\Integrations\Pipedrive;
 
+use App\Exceptions\InvalidApiKeyException;
+
 class PipedriveHelper
 {
     public static function demoSetByEmail(array $deal, string $demoSetByApiKey = null): string|null
     {
         $demoSetByApiKey = $demoSetByApiKey ?? env('PIPEDRIVE_DEMO_SET_BY');
+
+        if (! array_key_exists($demoSetByApiKey, $deal)) {
+            throw new InvalidApiKeyException('Invalid api key!');
+        }
 
         return $deal[$demoSetByApiKey]
         ? $deal[$demoSetByApiKey]['email'][0]['value']
--- tests/Unit/Integration/Pipedrive/PipedriverHelperDemoSetByEmailTest.php
+++ tests/Unit/Integration/Pipedrive/PipedriverHelperDemoSetByEmailTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Exceptions\InvalidApiKeyException;
 use App\Integrations\Pipedrive\PipedriveHelper;
 use App\Integrations\Pipedrive\PipedriveClientDummy;
 
@@ -14,3 +15,9 @@ it('returns null if it has no demo_set_by email', function () {
 
     expect(PipedriveHelper::demoSetByEmail($deals[2]))->toBe(null);
 });
+
+it('throws an exception if the provided api key is wrong', function () {
+    $deals = (new PipedriveClientDummy())->deals()->toArray();
+
+    expect(PipedriveHelper::demoSetByEmail($deals[2], 'invalid api key'))->toBe(null);
+})->throws(InvalidApiKeyException::class, 'Invalid api key!');
