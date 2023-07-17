11de8862a61b372c281e395a209f9dbe61e665f1
Paul Sochiera <paul@sochiera.com>
2023-07-17 16:42:04 +0200

    design

--- resources/js/Pages/CustomIntegrationField/Index.vue
+++ resources/js/Pages/CustomIntegrationField/Index.vue
@@ -65,13 +65,13 @@ const apiKeyRefs = useFieldsRef(props.available_integration_field_names, props.c
 </script>
 
 <template>
-    <div class="">
+    <div class="w-2/3">
         <PageHeader
             title="Custom Integration Fields"
-            :description="'To streamline all integration processes, we require you to create and use the custom fields you see below.\n\nThey need to be set to the respective API keys for the custom data field. Go to Pipedrive > Company Settings > Company > Datafields to see your custom fields and their API keys.\nYou can hover over a data field and click on the option dots to copy the key. Then you can paste it here and voilá, we will automatically integrate all your data.'"
+            :description="'To streamline all integration processes, we require you to create and use the custom fields you see below.\n\nThey need to be set to the respective API keys for the custom data field. Go to Pipedrive > Company Settings > Company > Datafields to see your custom fields and their API keys.\n\nYou can hover over a data field and click on the option dots to copy the key. Then you can paste it here and voilá, we will automatically integrate all your data.'"
         />
         <div
-            class="mr-96 flex items-center gap-5 rounded-md py-2"
+            class="flex items-center gap-5 rounded-md py-2"
             v-for="(integrationFieldName, index) in props.available_integration_field_names"
             :key="index"
         >
