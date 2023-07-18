60353f263cc8c9ab140d3a181e41883df67537bb
Paul Sochiera <paul@sochiera.com>
2023-07-18 16:36:13 +0200

    fix

--- database/factories/PlanFactory.php
+++ database/factories/PlanFactory.php
@@ -16,7 +16,7 @@ class PlanFactory extends Factory
         return [
             'name' => fake()->word().' Plan',
             'start_date' => Carbon::parse('-1 week'),
-            'target_amount_per_month' => 500000,
+            'target_amount_per_month' => 5_000_00,
             'target_variable' => TargetVariableEnum::ARR->value,
             'payout_frequency' => PayoutFrequencyEnum::MONTHLY->value,
             'organization_id' => Organization::factory()->create(),
