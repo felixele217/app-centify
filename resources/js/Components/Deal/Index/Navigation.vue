c68d3b7ca531d8d96c9430a673ecc47a3bb11b8c
Paul Sochiera <paul@sochiera.com>
2023-07-21 11:16:04 +0200

    design: navbar

--- resources/js/Components/Deal/Index/Navigation.vue
+++ resources/js/Components/Deal/Index/Navigation.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
 import queryParamValue from '@/utils/queryParamValue'
+import {
+    ClipboardDocumentCheckIcon,
+    ExclamationCircleIcon,
+    HandThumbDownIcon,
+    HandThumbUpIcon,
+} from '@heroicons/vue/24/outline'
 
 const tabs = [
     {
         name: 'All',
         href: route('deals.index'),
         current: queryParamValue('scope') === '',
+        icon: ClipboardDocumentCheckIcon,
     },
     {
         name: 'Open',
         href: route('deals.index') + '?scope=open',
         current: window.location.href.includes('open'),
+        icon: ExclamationCircleIcon,
     },
     {
         name: 'Accepted',
         href: route('deals.index') + '?scope=accepted',
         current: window.location.href.includes('accepted'),
+        icon: HandThumbUpIcon,
     },
     {
         name: 'Declined',
         href: route('deals.index') + '?scope=declined',
         current: window.location.href.includes('declined'),
+        icon: HandThumbDownIcon,
     },
 ]
 </script>
@@ -28,7 +38,7 @@ const tabs = [
 <template>
     <div>
         <nav
-            class="isolate flex w-144 divide-x divide-gray-200 rounded-lg shadow"
+            class="isolate flex w-128 divide-x divide-gray-200 rounded-lg shadow"
             aria-label="Tabs"
         >
             <a
@@ -39,11 +49,18 @@ const tabs = [
                     tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
                     tabIdx === 0 ? 'rounded-l-lg' : '',
                     tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
-                    'group relative min-w-0 flex-1 overflow-hidden bg-white px-4 py-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
+                    'group relative flex min-w-0 flex-1 items-center justify-center gap-2 overflow-hidden bg-white py-2.5 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
                 ]"
                 :aria-current="tab.current ? 'page' : undefined"
             >
+                <component
+                    :is="tab.icon"
+                    class="h-5 w-5 shrink-0"
+                    aria-hidden="true"
+                />
+
                 <span>{{ tab.name }}</span>
+
                 <span
                     aria-hidden="true"
                     :class="[tab.current ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-0.5']"
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,11 +4,11 @@ import ProfileDropdown from '@/Components/MainLayout/ProfileDropdown.vue'
 import SearchBar from '@/Components/MainLayout/SearchBar.vue'
 import Sidebar from '@/Components/MainLayout/Sidebar.vue'
 import Notification from '@/Components/Notification.vue'
-import { BellIcon, ChartPieIcon, FolderIcon, HomeIcon, PuzzlePieceIcon } from '@heroicons/vue/24/outline'
+import { BellIcon, ChartPieIcon, ClipboardDocumentCheckIcon, FolderIcon, HomeIcon, PuzzlePieceIcon } from '@heroicons/vue/24/outline'
 
 const navigation = [
     { name: 'Dashboard', href: route('dashboard'), icon: HomeIcon, current: route().current('dashboard') },
-    { name: 'Deals', href: route('deals.index'), icon: DealIcon, current: route().current('deals.index') },
+    { name: 'Deals', href: route('deals.index'), icon: ClipboardDocumentCheckIcon, current: route().current('deals.index') },
     { name: 'Plans', href: route('plans.index'), icon: ChartPieIcon, current: route().current('plans.index') },
     { name: 'Teams & Users', href: route('agents.index'), icon: FolderIcon, current: route().current('agents.index') },
     {
