4667ac85f81e2f9974daa27af9f410ad5a3c99c2
felixele217 <felix.droese@gmx.net>
2023-07-21 12:41:00 +0200

    remove: searchbar

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import ProfileDropdown from '@/Components/MainLayout/ProfileDropdown.vue'
-import SearchBar from '@/Components/MainLayout/SearchBar.vue'
 import Sidebar from '@/Components/MainLayout/Sidebar.vue'
 import Notification from '@/Components/Notification.vue'
 import {
-    BellIcon,
     ChartPieIcon,
     ClipboardDocumentCheckIcon,
     FolderIcon,
@@ -36,33 +33,7 @@ const navigation = [
         <Sidebar :navigation="navigation" />
 
         <div class="flex h-screen flex-col lg:pl-72">
-            <div
-                class="flex items-center gap-x-4 self-stretch border-b border-gray-200 bg-white px-8 py-3 shadow-sm lg:gap-x-6"
-            >
-                <SearchBar />
-
-                <div class="flex items-center gap-x-4 lg:gap-x-6">
-                    <button
-                        type="button"
-                        class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-                    >
-                        <span class="sr-only">View notifications</span>
-                        <BellIcon
-                            class="h-6 w-6"
-                            aria-hidden="true"
-                        />
-                    </button>
-
-                    <div
-                        class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-                        aria-hidden="true"
-                    />
-
-                    <ProfileDropdown />
-                </div>
-            </div>
-
-            <main class="grow p-5 sm:px-10 lg:px-16 lg:py-10">
+            <main class="grow p-5 sm:px-10 lg:p-10">
                 <slot />
             </main>
         </div>
