26341405659c70ff0bb291d641896a3b73020c71
Paul Sochiera <paul@sochiera.com>
2023-07-23 13:11:11 +0200

    feat: changed percentage input accordingly

--- resources/js/Components/Form/PercentageInput.vue
+++ resources/js/Components/Form/PercentageInput.vue
@@ -7,50 +7,22 @@ const props = defineProps<{
 
 const emit = defineEmits(['set-value'])
 
-function handleKeyDown(event: any) {
-    if (!['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace', 'Tab', '-'].includes(event.key)) {
-        return
-    }
-
-    if (event.key === '-') {
-        emit('set-value', -props.value)
-        return
-    }
-
-    if (event.key === 'Backspace') {
-        if (props.value < 0 && props.value.toString().length === 2) {
-            emit('set-value', 0)
-            return
-        }
-
-        emit('set-value', Number(props.value.toString().slice(0, -1)))
-        return
-    }
-
-    if (props.value >= 100) {
-        return
-    }
-
-    if (Number(props.value.toString() + event.key) >= 100) {
-        emit('set-value', 100)
-        return
-    }
-
-    if (props.value === 0) {
-        emit('set-value', Number(event.key))
+function handleChange(newValue: string) {
+    if (newValue.includes('%')) {
+        emit('set-value', parseInt(newValue.replace('%', '')))
+    } else if (newValue.length === 0) {
+        emit('set-value', 0)
     } else {
-        emit('set-value', Number(props.value.toString() + event.key))
+        emit('set-value', parseInt(newValue.slice(0, -1)))
     }
 }
-
-const percentageDisplay = (percentage: number) => `${Number(percentage)}%`
 </script>
 
 <template>
     <TextInput
         :class="props.value === 0 ? 'text-gray-300' : 'text-gray-900'"
-        @keydown.prevent="handleKeyDown"
+        @update:modelValue="handleChange"
         type="text"
-        :modelValue="percentageDisplay(props.value)"
+        :modelValue="props.value.toString() + '%'"
     />
 </template>
