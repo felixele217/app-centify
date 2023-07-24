347d1815fecd20db3b48ec547491e793aa14fd57
felixele217 <felix.droese@gmx.net>
2023-07-24 19:57:06 +0200

    feat: multi select no bwhitespace above and below first and last

--- resources/js/Components/Form/MultiSelect.vue
+++ resources/js/Components/Form/MultiSelect.vue
@@ -67,7 +67,7 @@ const selectedOptions = () => props.options.filter((option) => props.selectedIds
                 leave-to-class="opacity-0"
             >
                 <ListboxOptions
-                    class="absolute z-10 mt-1 max-h-60 w-2/3 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                    class="absolute z-10 mt-1 max-h-60 w-2/3 overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
                 >
                     <ListboxOption
                         as="template"
