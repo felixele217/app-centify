18527389ba2fb43e9fb5ef79a19bdc3a05dd5980
Paul Sochiera <paul@sochiera.com>
2023-07-24 10:55:34 +0200

    refactor paidleavecommission service

--- app/Services/Commission/PaidLeaveCommissionService.php
+++ app/Services/Commission/PaidLeaveCommissionService.php
@@ -5,22 +5,29 @@ namespace App\Services\Commission;
 use App\Enum\TimeScopeEnum;
 use App\Models\Agent;
 use App\Models\PaidLeave;
-use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 
 class PaidLeaveCommissionService
 {
+    private $dateInScope;
+
+    public function __construct()
+    {
+        $this->dateInScope = CarbonImmutable::now();
+    }
+
     public function calculate(Agent $agent, TimeScopeEnum $timeScope): int
     {
         $advancedQuery = match ($timeScope) {
             TimeScopeEnum::MONTHLY => $agent->paidLeaves()
-                ->whereMonth('end_date', Carbon::now()->month)
-                ->orWhereMonth('start_date', Carbon::now()->month),
+                ->whereMonth('end_date', $this->dateInScope->month)
+                ->orWhereMonth('start_date', $this->dateInScope->month),
             TimeScopeEnum::QUARTERLY => $agent->paidLeaves()
-                ->whereBetween('end_date', [Carbon::now()->firstOfQuarter(), Carbon::now()->lastOfQuarter()])
-                ->orWhereBetween('start_date', [Carbon::now()->firstOfQuarter(), Carbon::now()->lastOfQuarter()]),
+                ->whereBetween('end_date', [$this->dateInScope->firstOfQuarter(), $this->dateInScope->lastOfQuarter()])
+                ->orWhereBetween('start_date', [$this->dateInScope->firstOfQuarter(), $this->dateInScope->lastOfQuarter()]),
             TimeScopeEnum::ANNUALY => $agent->paidLeaves()
-                ->whereBetween('end_date', [Carbon::now()->firstOfYear(), Carbon::now()->lastOfYear()])
-                ->orWhereBetween('start_date', [Carbon::now()->firstOfYear(), Carbon::now()->lastOfYear()]),
+                ->whereBetween('end_date', [$this->dateInScope->firstOfYear(), $this->dateInScope->lastOfYear()])
+                ->orWhereBetween('start_date', [$this->dateInScope->firstOfYear(), $this->dateInScope->lastOfYear()]),
         };
 
         return intval(round($advancedQuery->get()
@@ -38,52 +45,22 @@ class PaidLeaveCommissionService
     private function paidLeaveWeekdaysForTimeScope(PaidLeave $paidLeave, TimeScopeEnum $timeScope): int
     {
         return match ($timeScope) {
-            TimeScopeEnum::MONTHLY => $this->paidLeaveWeekdaysForMonth($paidLeave),
-            TimeScopeEnum::QUARTERLY => $this->paidLeaveWeekdaysForQuarter($paidLeave),
-            TimeScopeEnum::ANNUALY => $this->paidLeaveWeekdaysForYear($paidLeave),
+            TimeScopeEnum::MONTHLY => $this->paidLeaveWeekdays($paidLeave, 'month', $this->dateInScope->firstOfMonth(), $this->dateInScope->lastOfMonth()),
+            TimeScopeEnum::QUARTERLY => $this->paidLeaveWeekdays($paidLeave, 'quarter', $this->dateInScope->firstOfQuarter(), $this->dateInScope->lastOfQuarter()),
+            TimeScopeEnum::ANNUALY => $this->paidLeaveWeekdays($paidLeave, 'year', $this->dateInScope->firstOfYear(), $this->dateInScope->lastOfYear()),
         };
     }
 
-    private function paidLeaveWeekdaysForMonth(PaidLeave $paidLeave): int
+    private function paidLeaveWeekdays(PaidLeave $paidLeave, string $timeScope, CarbonImmutable $timeScopeStartDate, CarbonImmutable $timeScopeEndDate): int
     {
-        $currentMonth = Carbon::now()->month;
+        $currentScope = data_get($this->dateInScope, $timeScope);
 
-        if ($paidLeave->start_date->month === $currentMonth && $paidLeave->end_date->month === $currentMonth) {
+        if (data_get($paidLeave->start_date, $timeScope) === $currentScope && data_get($paidLeave->end_date, $timeScope) === $currentScope) {
             return $paidLeave->end_date->diffInWeekdays($paidLeave->start_date) + 1;
-        } elseif ($paidLeave->end_date->month === $currentMonth) {
-            return $paidLeave->end_date->diffInWeekdays(Carbon::now()->firstOfMonth()) + 1;
-        } elseif ($paidLeave->start_date->month === $currentMonth) {
-            return $paidLeave->start_date->diffInWeekdays(Carbon::now()->lastOfMonth()) + 1;
-        }
-
-        return 0;
-    }
-
-    private function paidLeaveWeekdaysForQuarter(PaidLeave $paidLeave): int
-    {
-        $currentQuarter = Carbon::now()->quarter;
-
-        if ($paidLeave->start_date->quarter === $currentQuarter && $paidLeave->end_date->quarter === $currentQuarter) {
-            return $paidLeave->end_date->diffInWeekdays($paidLeave->start_date) + 1;
-        } elseif ($paidLeave->end_date->quarter === $currentQuarter) {
-            return $paidLeave->end_date->diffInWeekdays(Carbon::now()->firstOfQuarter()) + 1;
-        } elseif ($paidLeave->start_date->quarter === $currentQuarter) {
-            return $paidLeave->start_date->diffInWeekdays(Carbon::now()->lastOfQuarter()) + 1;
-        }
-
-        return 0;
-    }
-
-    private function paidLeaveWeekdaysForYear(PaidLeave $paidLeave): int
-    {
-        $currentYear = Carbon::now()->year;
-
-        if ($paidLeave->start_date->year === $currentYear && $paidLeave->end_date->year === $currentYear) {
-            return $paidLeave->end_date->diffInWeekdays($paidLeave->start_date) + 1;
-        } elseif ($paidLeave->end_date->year === $currentYear) {
-            return $paidLeave->end_date->diffInWeekdays(Carbon::now()->firstOfYear()) + 1;
-        } elseif ($paidLeave->start_date->year === $currentYear) {
-            return $paidLeave->start_date->diffInWeekdays(Carbon::now()->lastOfYear()) + 1;
+        } elseif (data_get($paidLeave->end_date, $timeScope) === $currentScope) {
+            return $paidLeave->end_date->diffInWeekdays($timeScopeStartDate) + 1;
+        } elseif (data_get($paidLeave->start_date, $timeScope) === $currentScope) {
+            return $paidLeave->start_date->diffInWeekdays($timeScopeEndDate) + 1;
         }
 
         return 0;
