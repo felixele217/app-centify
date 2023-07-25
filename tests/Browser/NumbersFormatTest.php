47dfaac6ff5334c6d23dd26da8c8d6843e8ae6b9
Paul Sochiera <paul@sochiera.com>
2023-07-25 09:14:22 +0200

    feat: dusk test to assert correct number formats (euros & percentage)

---
+++ tests/Browser/NumbersFormatTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\Admin;
+use App\Models\Agent;
+use App\Models\Deal;
+use App\Models\Plan;
+use Carbon\Carbon;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class NumbersFormatTest extends DuskTestCase
+{
+    public function testFormatsEuros(): void
+    {
+        $admin = $this->setupDatabase();
+
+        $this->browse(function (Browser $browser) use($admin) {
+            $browser->loginAs($admin)
+                ->visit(route('dashboard'))
+                ->assertUrlIs(route('dashboard'))
+                ->waitForText('Total Payout')
+                ->assertSee('5.000,00€');
+        });
+    }
+
+    public function testFormatsPercentage(): void
+    {
+        $admin = $this->setupDatabase();
+
+        $this->browse(function (Browser $browser) use($admin) {
+            $browser->loginAs($admin)
+                ->visit(route('dashboard'))
+                ->assertUrlIs(route('dashboard'))
+                ->waitForText('Quota Attainment')
+                ->assertSee('50%');
+        });
+    }
+
+    private function setupDatabase(): Admin
+    {
+        $admin = Admin::factory()->create();
+
+        $plan = Plan::factory()->create([
+            'organization_id' => $admin->organization->id,
+            'creator_id' => $admin->id,
+            'target_amount_per_month' => 10_000_00,
+        ]);
+
+        $agent = Agent::factory()->create([
+            'organization_id' => $admin->organization->id,
+            'base_salary' => 50_000_00,
+            'on_target_earning' => 170_000_00
+        ]);
+
+        $plan->agents()->attach($agent);
+
+        Deal::factory()->create([
+           'value' => 5_000_00,
+           'agent_id' => $agent->id,
+           'owner_email' => $agent->email,
+           'accepted_at' => Carbon::yesterday(),
+        ]);
+
+        return $admin;
+    }
+}
