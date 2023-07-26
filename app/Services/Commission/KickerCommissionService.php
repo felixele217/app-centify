e9974f1c7f0449a84c893b88c88c3e353b82f9c0
Paul Sochiera <paul@sochiera.com>
2023-07-26 12:49:37 +0200

    refactor

--- app/Services/Commission/KickerCommissionService.php
+++ app/Services/Commission/KickerCommissionService.php
@@ -9,26 +9,14 @@ class KickerCommissionService
 {
     public function calculate(Agent $agent, TimeScopeEnum $timeScope, float $quotaAttainment): int
     {
-        if ($this->reachedQuarterlyTargetWithinTheCurrentMonth($agent, $timeScope, $quotaAttainment)
-        || $this->reachedQuarterlyTargetWithinQuarter($agent, $timeScope, $quotaAttainment)) {
-            $kickerCommission = $agent->plans()->active()->first()?->kicker?->payout_in_percent * ($agent->base_salary / 12);
+        $kicker = $agent->plans()->active()->first()->kicker;
+
+        $factor = $timeScope === TimeScopeEnum::MONTHLY ? 3 : 1;
+
+        if ($kicker?->threshold_in_percent * $factor <= $quotaAttainment) {
+            $kickerCommission = $kicker?->payout_in_percent * ($agent->base_salary / 12);
         }
 
         return intval(round($kickerCommission ?? 0));
     }
-
-    private function reachedQuarterlyTargetWithinTheCurrentMonth(Agent $agent, TimeScopeEnum $timeScope, float $quotaAttainment): bool
-    {
-        return $timeScope === TimeScopeEnum::MONTHLY && $agent->plans()->active()->first()?->kicker?->threshold_in_percent * 3 <= $quotaAttainment;
-    }
-
-    private function reachedQuarterlyTargetWithinQuarter(Agent $agent, TimeScopeEnum $timeScope, float $quotaAttainment): bool
-    {
-        return $timeScope === TimeScopeEnum::QUARTERLY && $agent->plans()->active()->first()?->kicker?->threshold_in_percent <= $quotaAttainment;
-    }
-
-    // private function reachedQuarterlyTargetWithinSingleMonth(Agent $agent, TimeScopeEnum $timeScope): bool
-    // {
-
-    // }
 }
