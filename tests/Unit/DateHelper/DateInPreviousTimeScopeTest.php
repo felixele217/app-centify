8767533b334de2d644ed3a692aed046f69bf4d02
Paul Sochiera <paul@sochiera.com>
2023-07-28 09:36:13 +0200

    feat: previous time scope helper function for all time scopes

--- tests/Unit/DateHelper/DateInPreviousTimeScopeTest.php
+++ tests/Unit/DateHelper/DateInPreviousTimeScopeTest.php
@@ -3,8 +3,14 @@
 use App\Enum\TimeScopeEnum;
 use App\Helper\DateHelper;
 
-it('returns immutable date in previous specified timescope', function () {
-    $timeScope = TimeScopeEnum::MONTHLY;
+it('returns immutable date in previous specified timescope for monthly', function () {
+    expect(DateHelper::dateInPreviousTimeScope(TimeScopeEnum::MONTHLY)->isLastMonth())->toBeTrue();
+});
 
-    expect(DateHelper::dateInPreviousTimeScope($timeScope)->isLastQuarter())->toBeTrue();
+it('returns immutable date in previous specified timescope for quarterly', function () {
+    expect(DateHelper::dateInPreviousTimeScope(TimeScopeEnum::QUARTERLY)->isLastQuarter())->toBeTrue();
+});
+
+it('returns immutable date in previous specified timescope for annualy', function () {
+    expect(DateHelper::dateInPreviousTimeScope(TimeScopeEnum::ANNUALY)->isLastQuarter())->toBeTrue();
 });
