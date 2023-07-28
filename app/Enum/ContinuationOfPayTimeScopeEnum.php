f95a6e13130341f5d5226a3988024d62b2b65f6c
Paul Sochiera <paul@sochiera.com>
2023-07-28 15:00:52 +0200

    extended Continuation of pay time scope

--- app/Enum/ContinuationOfPayTimeScopeEnum.php
+++ app/Enum/ContinuationOfPayTimeScopeEnum.php
@@ -7,11 +7,15 @@ namespace App\Enum;
 enum ContinuationOfPayTimeScopeEnum: string
 {
     case QUARTER = 'last 13 weeks';
+    case SEMI_YEAR = 'last 6 months';
+    case YEAR = 'last year';
 
     public function amountOfDays(): int
     {
         return match ($this->name) {
             self::QUARTER->name => 91,
+            self::SEMI_YEAR->name => 180,
+            self::YEAR->name => 365,
         };
     }
 }
--- resources/js/types/Enum/ContinuationOfPayTimeScopeEnum.d.ts
+++ resources/js/types/Enum/ContinuationOfPayTimeScopeEnum.d.ts
@@ -1,1 +1,1 @@
-export type ContinuationOfPayTimeScopeEnum = 'last 13 weeks'
+export type ContinuationOfPayTimeScopeEnum = 'last 13 weeks' | 'last 6 months' | 'last year'
