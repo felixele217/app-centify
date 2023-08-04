f78286641d8532c76d7e73dbd15cbc083f2586f5
felixele217 <felix.droese@gmx.net>
2023-08-04 20:04:18 +0200

    proper dot placement

--- resources/js/Pages/Integration/CustomField/Index.vue
+++ resources/js/Pages/Integration/CustomField/Index.vue
@@ -90,8 +90,8 @@ const apiKeyRefs = useCustomFieldRefs(props.available_custom_field_names, props.
             <p class="">2.</p>
             <div class="">
                 You should see a green
-                <span class="font-bold"> 'Add custom field'. </span>
-                button
+                <span class="font-bold"> 'Add custom field'</span>
+                button.
                 <br />
                 Create the fields which types (e.g. Person field) and names (e.g. 'demo_set_by') match the ones below.
             </div>
