969c59a050154095ee6d8761bc6931abea01d3ef
Paul Sochiera <paul@sochiera.com>
2023-08-08 13:29:04 +0200

    t7

--- .github/scripts/wait-for-deployment.sh
+++ .github/scripts/wait-for-deployment.sh
@@ -8,11 +8,6 @@ for i in {1..30}; do # Try for 5 minutes
     deployed_version_json=$(curl -s https://staging.centify.de/deployed-version)
     deployed_version=$(echo "$deployed_version_json" | jq -r '.version')
 
-    echo $deployed_version
-    echo $deployed_version_json
-    echo expected_version
-    echo $version
-
     if [[ "$deployed_version" == "$version" ]]; then
         echo "Deployment complete"
         exit 0
