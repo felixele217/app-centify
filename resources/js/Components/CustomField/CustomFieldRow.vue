41be220d885035a07826ceae1268f0000e5ed601
Paul Sochiera <paul@sochiera.com>
2023-08-09 10:10:05 +0200

    refactor: CustomFieldRow

---
+++ resources/js/Components/CustomField/CustomFieldRow.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import CustomField from '@/types/CustomField'
+import { CustomFieldEnum } from '@/types/Enum/CustomFieldEnum'
+import Integration from '@/types/Integration'
+import customField from '@/utils/CustomField/customField'
+import notify from '@/utils/notify'
+import { router } from '@inertiajs/vue3'
+import { ref } from 'vue'
+import SecondaryButton from '../Buttons/SecondaryButton.vue'
+import TextInput from '../Form/TextInput.vue'
+
+const props = defineProps<{
+    customFieldName: CustomFieldEnum
+    integration: Integration
+}>()
+
+function upsertCustomField(customFieldName: CustomFieldEnum) {
+    const currentCustomField = customField(props.integration.custom_fields!, customFieldName)
+
+    if (currentCustomField) {
+        updateCustomField(currentCustomField)
+    } else {
+        storeCustomField(customFieldName)
+    }
+}
+
+function storeCustomField(customFieldName: CustomFieldEnum) {
+    router.post(
+        route('integrations.custom-fields.store', props.integration.id),
+        {
+            name: customFieldName,
+            api_key: apiKey.value,
+            integration_type: props.integration.name,
+        },
+        {
+            onSuccess: () => {
+                notify('Api key stored!', 'We can now access the value of your custom field.')
+            },
+            onError: () => {
+                notify(
+                    'Store failed!',
+                    'The format of the api key was invalid. It has to be 40 characters long.',
+                    false
+                )
+            },
+        }
+    )
+}
+
+function updateCustomField(customField: CustomField) {
+    router.put(
+        route('integrations.custom-fields.update', [customField.integration_id, customField.id]),
+        {
+            api_key: apiKey.value,
+        },
+        {
+            onSuccess: () => {
+                notify('Api key updated!', 'We can now access the value of your custom field.')
+            },
+            onError: () => {
+                notify(
+                    'Update failed!',
+                    'The format of the api key was invalid. It has to be 40 characters long.',
+                    false
+                )
+            },
+        }
+    )
+}
+
+const isEditing = ref<boolean>(false)
+const apiKey = ref<string>(customField(props.integration.custom_fields!, props.customFieldName).api_key)
+</script>
+
+<template>
+    <div class="flex items-center gap-5 rounded-md py-2">
+        <p class="whitespace-nowrap">{{ customFieldName }}:</p>
+
+        <TextInput
+            type="text"
+            v-model="apiKey"
+            class="ml-3"
+            no-top-margin
+        />
+
+
+        <SecondaryButton
+        
+            @click="upsertCustomField(customFieldName)"
+            text="Save"
+        />
+    </div>
+</template>
--- resources/js/Pages/Integration/CustomField/Index.vue
+++ resources/js/Pages/Integration/CustomField/Index.vue
@@ -1,74 +1,14 @@
 <script setup lang="ts">
 import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue'
 import Card from '@/Components/Card.vue'
-import useCustomFieldRefs from '@/Components/CustomField/Composable/useCustomFieldRefs'
-import TextInput from '@/Components/Form/TextInput.vue'
-import CustomField from '@/types/CustomField'
+import CustomFieldRow from '@/Components/CustomField/CustomFieldRow.vue'
 import { CustomFieldEnum } from '@/types/Enum/CustomFieldEnum'
 import Integration from '@/types/Integration'
-import customField from '@/utils/CustomField/customField'
-import notify from '@/utils/notify'
-import { router } from '@inertiajs/vue3'
 
 const props = defineProps<{
     integration: Integration
     available_custom_field_names: Array<CustomFieldEnum>
 }>()
-
-function upsertCustomField(customFieldName: CustomFieldEnum) {
-    const currentCustomField = customField(props.integration.custom_fields!, customFieldName)
-
-    if (currentCustomField) {
-        updateCustomField(currentCustomField)
-    } else {
-        storeCustomField(customFieldName)
-    }
-}
-
-function storeCustomField(customFieldName: CustomFieldEnum) {
-    router.post(
-        route('integrations.custom-fields.store', props.integration.id),
-        {
-            name: customFieldName,
-            api_key: apiKeyRefs.value[customFieldName],
-            integration_type: props.integration.name,
-        },
-        {
-            onSuccess: () => {
-                notify('Api key stored!', 'We can now access the value of your custom field.')
-            },
-            onError: () => {
-                notify(
-                    'Store failed!',
-                    'The format of the api key was invalid. It has to be 40 characters long.',
-                    false
-                )
-            },
-        }
-    )
-}
-
-function updateCustomField(customField: CustomField) {
-    router.put(
-        route('integrations.custom-fields.update', [customField.integration_id, customField.id]),
-        {
-            api_key: apiKeyRefs.value[customField.name],
-        },
-        {
-            onSuccess: () => {
-                notify('Api key updated!', 'We can now access the value of your custom field.')
-            },
-            onError: () => {
-                notify(
-                    'Update failed!',
-                    'The format of the api key was invalid. It has to be 40 characters long.',
-                    false
-                )
-            },
-        }
-    )
-}
-const apiKeyRefs = useCustomFieldRefs(props.available_custom_field_names, props.integration.custom_fields!)
 </script>
 
 <template>
@@ -100,25 +40,18 @@ const apiKeyRefs = useCustomFieldRefs(props.available_custom_field_names, props.
         </Card>
 
         <Card class="mt-6">
-            <h3>Your Integration API Keys</h3>
+            <div class="mb-8 flex justify-between">
+                <h3>Your Integration API Keys</h3>
 
-            <div
-                class="flex items-center gap-5 rounded-md py-2"
+                <PrimaryButton text="Test & Sync" />
+            </div>
+
+            <CustomFieldRow
                 v-for="(customFieldName, index) in props.available_custom_field_names"
                 :key="index"
-            >
-                <p class="whitespace-nowrap">{{ customFieldName }}:</p>
-                <TextInput
-                    type="text"
-                    v-model="apiKeyRefs[customFieldName]"
-                    class="ml-3"
-                    no-top-margin
-                />
-                <PrimaryButton
-                    @click="upsertCustomField(customFieldName)"
-                    text="Save"
-                />
-            </div>
+                :custom-field-name="customFieldName"
+                :integration="props.integration"
+            />
         </Card>
     </div>
 </template>
