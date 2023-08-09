1f9193a027362741ecb723748d1593d1db3e1ab0
Paul Sochiera <paul@sochiera.com>
2023-08-09 13:06:40 +0200

    feat: more thorough tests for commission from quota

--- tests/Unit/Agent/Commission/CommissionFromQuotaServiceTest.php
+++ tests/Unit/Agent/Commission/CommissionFromQuotaServiceTest.php
@@ -4,19 +4,25 @@ use App\Enum\TimeScopeEnum;
 use App\Models\Agent;
 use App\Services\Commission\CommissionFromQuotaService;
 
-it('calculates the commission correctly for the respective scopes', function (TimeScopeEnum $timeScope) {
+it('calculates the commission correctly for the respective scopes', function (TimeScopeEnum $timeScope, float $quotaAttainment) {
     $agent = Agent::factory()->create([
         'base_salary' => $baseSalary = 80_000_00,
         'on_target_earning' => $onTargetEarning = 200_000_00,
     ]);
 
-    $expectedCommission = (($onTargetEarning - $baseSalary) / (12 / $timeScope->monthCount()));
+    $expectedCommissionForFullQuota = (($onTargetEarning - $baseSalary) / (12 / $timeScope->monthCount()));
 
-    expect((new CommissionFromQuotaService())->calculate($agent, $timeScope, 1))->toBe($expectedCommission);
+    expect((new CommissionFromQuotaService())->calculate($agent, $timeScope, $quotaAttainment))->toBe(intval($expectedCommissionForFullQuota * $quotaAttainment));
 })->with([
-    TimeScopeEnum::MONTHLY,
-    TimeScopeEnum::QUARTERLY,
-    TimeScopeEnum::ANNUALY,
+    [TimeScopeEnum::MONTHLY, 1],
+    [TimeScopeEnum::QUARTERLY, 1],
+    [TimeScopeEnum::ANNUALY, 1],
+
+    [TimeScopeEnum::MONTHLY, 0.3],
+    [TimeScopeEnum::QUARTERLY, 0.4],
+    [TimeScopeEnum::ANNUALY, 1.3],
+    [TimeScopeEnum::ANNUALY, 2.5],
+    [TimeScopeEnum::ANNUALY, 0.7],
 ]);
 
 it('calculates the commission correctly for the respective scopes if the quota attainment in the timescope was null', function (TimeScopeEnum $timeScope) {
