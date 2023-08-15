35378e57a3c74198c54974d20b6159bf0d38bc4b
felixele217 <felix.droese@gmx.net>
2023-08-15 13:10:14 +0200

    feat: syncing without a connection throws an error

---
+++ app/Exceptions/SyncWithoutConnectionException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class SyncWithoutConnectionException extends Exception
+{
+    public function __construct()
+    {
+        $this->message = 'You are not connected to the integration! Please connect before trying to synchronize the deals.';
+    }
+}
---
+++ tests/Feature/Integration/Pipedrive/PipedriveIntegrationService/SyncWithoutConnectionTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Integrations\Pipedrive\PipedriveIntegrationService;
+
+it('throws an exception if trying to sync without an active connection', function () {
+    $admin = signInAdmin();
+
+    (new PipedriveIntegrationService($admin->organization))->syncAgentDeals();
+})->throws(SyncWithoutConnectionException::class);
