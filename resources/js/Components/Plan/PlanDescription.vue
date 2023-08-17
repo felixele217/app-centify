7ac7b8d7b881dddb7f9e3029c660127032e0acd2
Paul Sochiera <paul@sochiera.com>
2023-08-17 12:38:16 +0200

    feat: plan description

--- resources/js/Components/Plan/PlanDescription.vue
+++ resources/js/Components/Plan/PlanDescription.vue
@@ -14,28 +14,58 @@ const props = defineProps<{
         <p class="text-base font-semibold">Description</p>
 
         <p class="mt-2">
-            The plan
+            The
+            <span class="font-semibold">{{ props.form.plan_cycle || '{interval}' }}</span>
+            plan
             <span class="font-semibold">{{ props.form.name || '{plan_name}' }}</span>
-            will be renewed
-            <span class="font-semibold">{{ props.form.plan_cycle || '{interval}' }},</span>
-            starting on
+            is assigned to
+            <span class="font-semibold">{{ props.form.assigned_agent_ids.length }}</span>
+            agent(s) and starts on
             <span class="font-semibold">{{
                 formatDate(form.start_date) !== '-' ? formatDate(props.form.start_date) : '' || '{start_date}'
-            }}</span>
-            and is assigned to
-            <span class="font-semibold">{{ props.form.assigned_agent_ids.length }}</span>
-            agent(s).
+            }}</span
+            >.
         </p>
 
         <p class="mt-2">
-            The variable
+            Your agents are incentivised on
             <span class="font-semibold">{{ props.form.target_variable || '{target_variable}' }}</span>
-            is attributed to the plan and triggered by
-            <span class="font-semibold">{{ props.form.trigger || '{trigger}' }}.</span>
+            as the target variable. The individual monthly target is set to
+            <span class="font-semibold">{{
+                euroDisplay(props.form.target_amount_per_month, 0) || '{target_amount}'
+            }}</span>
+            yielding a 100% quota attainment.
+            <br />
+            A change of
+            <span class="font-semibold">{{ props.form.trigger || '{trigger}' }}</span>
+            within your CRM system triggers the target variable into the plan.
 
-            For this variable, you set the individual monthly target at
-            <span class="font-semibold"
-                >{{ euroDisplay(props.form.target_amount_per_month, 0) || '{target_amount_per_month}' }}.
+            <span v-if="props.form.cap">
+                <br />
+                The target variable is capped to an amount of
+                <span class="font-semibold">{{ euroDisplay(props.form.cap, 0) || '{cap_value}' }}</span>
+                per
+                <span class="font-semibold">{{ props.form.target_variable || '{target_variable}' }}</span
+                >.
+            </span>
+        </p>
+
+        <p class="mt-2">
+            <span v-if="props.form.cliff.threshold_in_percent">
+                An agent must achieve at least
+                <span class="font-semibold">{{ props.form.cliff.threshold_in_percent || '{cliff_value}' }}</span
+                >% of the quota to qualify for a commission.
+            </span>
+
+            <span v-if="props.form.kicker.threshold_in_percent && props.form.kicker.payout_in_percent">
+                <br />
+
+                An agent must achieve at least
+                <span class="font-semibold">{{ props.form.kicker.threshold_in_percent || '{kicker_treshhold}' }}</span
+                >% of the quota to earn a kicker equal to
+                <span class="font-semibold">{{ props.form.kicker.payout_in_percent || '{kicker_payout}' }}</span
+                >% of <span class="font-semibold">{{ props.form.kicker.salary_type || '{salary_type}' }}</span
+                >.
             </span>
         </p>
     </div>
