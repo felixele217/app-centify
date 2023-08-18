254fa221da23e290b39d9ea1648c858cb18930e4
felixele217 <felix.droese@gmx.net>
2023-08-18 14:09:51 +0200

    design: responsibility of last column in deal table

--- resources/js/Components/Deal/Index/DealStatus.vue
+++ resources/js/Components/Deal/Index/DealStatus.vue
@@ -19,14 +19,19 @@ const props = defineProps<{
         class="flex justify-center gap-2 text-gray-500"
         v-if="!props.deal.accepted_at && !props.deal.active_rejection?.created_at"
     >
-        <HandThumbUpIcon
-            @click="$emit('accepted', deal.id)"
-            class="h-6 w-6 cursor-pointer hover:text-green-500"
-        />
-        <HandThumbDownIcon
-            @click="$emit('rejected', deal.id)"
-            class="h-6 w-6 cursor-pointer hover:text-red-600"
-        />
+        <div>
+            <HandThumbUpIcon
+                @click="$emit('accepted', deal.id)"
+                class="h-6 w-6 cursor-pointer hover:text-green-500"
+            />
+        </div>
+
+        <div>
+            <HandThumbDownIcon
+                @click="$emit('rejected', deal.id)"
+                class="h-6 w-6 cursor-pointer hover:text-red-600"
+            />
+        </div>
     </div>
 
     <AcceptedOrRejectedBadge
