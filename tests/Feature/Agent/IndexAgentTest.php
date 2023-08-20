b0b27c18d079c87b6d534998980a726fa60bdb7f
felixele217 <felix.droese@gmx.net>
2023-08-20 12:21:29 +0200

    feat: it properly sends all plans to deals index

--- tests/Feature/Agent/IndexAgentTest.php
+++ tests/Feature/Agent/IndexAgentTest.php
@@ -3,12 +3,17 @@
 use App\Enum\AgentStatusEnum;
 use App\Enum\ContinuationOfPayTimeScopeEnum;
 use App\Models\Agent;
+use App\Models\Plan;
 use Inertia\Testing\AssertableInertia;
 
 it('passes the correct props', function () {
     $admin = signInAdmin();
 
     $agents = Agent::factory(3)->hasPaidLeaves(1)->create([
+        'organization_id' => $admin->organization->id,
+    ]);
+
+    Plan::factory($planCount = 3)->create([
         'organization_id' => $admin->organization->id,
     ]);
 
@@ -19,20 +24,24 @@ it('passes the correct props', function () {
                 ->has('agents', $agents->count())
                 ->has('agents.0.active_paid_leave')
                 ->has('agents.0.active_plans')
+                ->has('plans', $planCount)
                 ->where('possible_statuses', array_column(AgentStatusEnum::cases(), 'value'))
                 ->where('continuation_of_pay_time_scope_options', array_column(ContinuationOfPayTimeScopeEnum::cases(), 'value'))
         );
 });
 
-it('does not send foreign agents', function () {
+it('does not send foreign props', function () {
     signInAdmin();
 
     Agent::factory(3)->create();
+
+    Plan::factory(3)->create();
 
     $this->get(route('agents.index'))
         ->assertInertia(
             fn (AssertableInertia $page) => $page
                 ->component('Agent/Index')
                 ->has('agents', 0)
+                ->has('plans', 0)
         );
 });
