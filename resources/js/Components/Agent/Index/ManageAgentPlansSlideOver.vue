b8b614f196c16d3c74eba616053a94a3c097ba03
felixele217 <felix.droese@gmx.net>
2023-08-20 12:37:26 +0200

    feat: add checkboxes to plan names

--- resources/js/Components/Agent/Index/ManageAgentPlansSlideOver.vue
+++ resources/js/Components/Agent/Index/ManageAgentPlansSlideOver.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
+import Checkbox from '@/Components/Form/Checkbox.vue'
 import SlideOver from '@/Components/SlideOver.vue'
 import Agent from '@/types/Agent'
 import Plan from '@/types/Plan/Plan'
-import notify from '@/utils/notify'
-import { useForm } from '@inertiajs/vue3'
-import { watch } from 'vue'
 
 const emit = defineEmits<{
     'close-slide-over': []
 }>()
-
-function closeSlideOver() {
-    form.reset()
-
-    emit('close-slide-over')
-}
 
 const props = defineProps<{
     isOpen: boolean
@@ -22,62 +14,32 @@ const props = defineProps<{
     plans: Array<Pick<Plan, 'id' | 'name'>>
 }>()
 
-const form = useForm({
-    name: '',
-    email: '',
-    base_salary: null as number | null,
-    on_target_earning: null as number | null,
-})
-
-watch(
-    () => props.agent,
-    async (agent) => {
-        if (agent) {
-            form.name = agent.name
-            form.email = agent.email
-            form.base_salary = agent.base_salary ?? null
-            form.on_target_earning = agent.on_target_earning ?? null
-        }
+function agentIsAssignedToPlan(planName: string) {
+    if (props.agent?.active_plans?.map((active_plan) => active_plan.name).includes(planName)) {
+        return true
     }
-)
 
-function submit() {
-    if (props.agent) {
-        form.put(route('agents.update', props.agent.id), {
-            onSuccess: () => {
-                closeSlideOver()
+    return false
+}
 
-                notify(
-                    'Agent updated',
-                    'Your agent still has the same deals as before, but be aware of errors when trying to sync new data if you changed the email.'
-                )
-            },
-        })
-    } else {
-        form.post(route('agents.store'), {
-            onSuccess: () => {
-                closeSlideOver()
-
-                notify(
-                    'Agent created',
-                    "This agent's metrics now count towards your totals.\n You should assign this agent a plan now."
-                )
-            },
-        })
-    }
+function handleUpdate() {
+    console.log('hallo')
 }
 </script>
 
 <template>
     <SlideOver
         :is-open="props.isOpen"
-        @close-slide-over="closeSlideOver"
-        @submit="submit"
+        @close-slide-over="emit('close-slide-over')"
         title="Manage Plans"
         description="You can manage which plans will affect this agent's commission."
     >
         <p v-for="plan in props.plans">
-            {{ plan.name }}
+            <Checkbox
+                :label="plan.name"
+                :checked="agentIsAssignedToPlan(plan.name)"
+                @update:checked="handleUpdate()"
+            />
         </p>
     </SlideOver>
 </template>
