d60ccf509ff71f9d10dc68b45bc743392672f8b5
felixele217 <felix.droese@gmx.net>
2023-08-30 13:18:08 +0200

    design: plan description whitespaces

--- resources/js/Components/Plan/PlanDescription.vue
+++ resources/js/Components/Plan/PlanDescription.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Agent from '@/types/Agent'
 import { UpsertPlanForm } from '@/types/Form/UpsertPlanForm'
 import formatDate from '@/utils/Date/formatDate'
 import euroDisplay from '@/utils/euroDisplay'
@@ -35,44 +34,52 @@ const props = defineProps<{
                 euroDisplay(props.form.target_amount_per_month, 0) || '{target_amount}'
             }}</span>
             yielding a 100% quota attainment.
-            <br />
-            A change of
-            <span class="font-semibold">{{ props.form.trigger || '{trigger}' }}</span>
-            within your CRM system triggers the target variable into the plan.
-
-            <span v-if="props.form.cap">
-                <br />
-                The target variable is capped to an amount of
-                <span class="font-semibold">{{ euroDisplay(props.form.cap, 0) || '{cap_value}' }}</span>
-                per
-                <span class="font-semibold">{{ props.form.target_variable || '{target_variable}' }}</span
-                >.
-            </span>
         </p>
 
         <p class="mt-2">
-            <span v-if="props.form.cliff.threshold_in_percent">
-                An agent must achieve at least
-                <span class="font-semibold">{{ props.form.cliff.threshold_in_percent || '{cliff_value}' }}</span
-                >% of the quota to qualify for a commission.
-            </span>
+            A change of
+            <span class="font-semibold">{{ props.form.trigger || '{trigger}' }}</span>
+            within your CRM system triggers the target variable into the plan.
+        </p>
 
-            <span
-                v-if="
-                    props.form.kicker.threshold_in_percent ||
-                    props.form.kicker.payout_in_percent ||
-                    props.form.kicker.salary_type
-                "
-            >
-                <br />
+        <p
+            class="mt-2"
+            v-if="props.form.cap"
+        >
+            The target variable <span class="font-semibold">is capped</span> to an amount of
+            <span class="font-semibold">{{ euroDisplay(props.form.cap, 0) || '{cap_value}' }}</span>
+            per
+            <span class="font-semibold">{{ props.form.target_variable || '{target_variable}' }}</span
+            >.
+        </p>
 
-                An agent must achieve at least
-                <span class="font-semibold">{{ props.form.kicker.threshold_in_percent || '{kicker_treshhold}' }}</span
-                >% of the quota to earn a kicker equal to
-                <span class="font-semibold">{{ props.form.kicker.payout_in_percent || '{kicker_payout}' }}</span
-                >% of <span class="font-semibold">{{ props.form.kicker.salary_type || '{salary_type}' }}</span
-                >.
+        <p
+            class="mt-2"
+            v-if="props.form.cliff.threshold_in_percent"
+        >
+            An agent must
+            <span class="font-semibold"
+                >achieve at least {{ props.form.cliff.threshold_in_percent || '{cliff_value}' }}
             </span>
+            % of the quota
+            <span class="font-semibold"> to qualify for a commission. </span>
+        </p>
+
+        <p
+            class="mt-2"
+            v-if="
+                props.form.kicker.threshold_in_percent ||
+                props.form.kicker.payout_in_percent ||
+                props.form.kicker.salary_type
+            "
+        >
+            An agent must
+            <span class="font-semibold"
+                >achieve at least {{ props.form.kicker.threshold_in_percent || '{kicker_treshhold}' }}</span
+            >% of the quota to earn a kicker equal to
+            <span class="font-semibold">{{ props.form.kicker.payout_in_percent || '{kicker_payout}' }}</span
+            >% of <span class="font-semibold">{{ props.form.kicker.salary_type || '{salary_type}' }}</span
+            >.
         </p>
     </div>
 </template>
