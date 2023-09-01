45374e627cc6d441ce0f57572c42b31d6f2bba2b
felixele217 <droese.felix@gmail.com>
2023-09-01 13:14:52 +0200

    ui/ux: can submit modal with pressing enter

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -33,7 +33,10 @@ const props = defineProps<{
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
             </TransitionChild>
 
-            <div class="fixed inset-0 z-50 overflow-y-auto">
+            <div
+                @keyup.enter="$emit('modal-action')"
+                class="fixed inset-0 z-50 overflow-y-auto"
+            >
                 <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <TransitionChild
                         as="template"
