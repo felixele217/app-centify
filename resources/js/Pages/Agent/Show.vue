42a2576796634833df6b295af67e9acc4079c0f1
felixele217 <droese.felix@gmail.com>
2023-09-05 14:13:19 +0200

    feat: base salary and ote for agent show

--- resources/js/Pages/Agent/Show.vue
+++ resources/js/Pages/Agent/Show.vue
@@ -3,6 +3,7 @@ import Filter from '@/Components/Form/Filter.vue'
 import Agent from '@/types/Agent'
 import { TimeScopeEnum } from '@/types/Enum/TimeScopeEnum'
 import currentScope from '@/utils/Date/currentScope'
+import euroDisplay from '@/utils/euroDisplay'
 import queryParamValue from '@/utils/queryParamValue'
 
 const props = defineProps<{
@@ -13,12 +14,19 @@ const timeScopeFromQuery = queryParamValue('time_scope') as TimeScopeEnum | ''
 </script>
 
 <template>
-    <div class="flex justify-between gap-5">
-        <div>
-            <h1>{{ agent.name }} Deep Dive</h1>
-            <p class="font-semibold text-gray-400">{{ currentScope(timeScopeFromQuery) }}</p>
+    <div class="w-216">
+        <div class="flex justify-between gap-5">
+            <h1>{{ agent.name }}</h1>
+            <div>
+                <Filter :reload-url="route('agents.show', props.agent.id)" />
+                <p class="font-semibold text-gray-400">{{ currentScope(timeScopeFromQuery) }}</p>
+            </div>
         </div>
-
-        <Filter :reload-url="route('agents.show', props.agent.id)" />
+        <div class="mt-5 grid w-80 grid-cols-2 space-y-0.5 text-gray-600">
+            <p class="font-semibold text-gray-900">Base Salary:</p>
+            <p class="text-right tabular-nums">{{ euroDisplay(agent.base_salary) }}</p>
+            <p class="font-semibold text-gray-900">On Target Earning:</p>
+            <p class="text-right tabular-nums">{{ euroDisplay(agent.on_target_earning) }}</p>
+        </div>
     </div>
 </template>
